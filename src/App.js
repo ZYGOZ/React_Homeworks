1665bfc130bee88a6615417192fd78df1dc2aa3f
ZYGOZ <Kamrangametest@gmail.com>
2023-04-07 23:24:49 +0400

    Footer First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import Footer from "./Components/Footer.js"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
